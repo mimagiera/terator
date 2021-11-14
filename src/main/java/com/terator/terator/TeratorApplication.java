9ff6bdf2a816b91865fcd1b1085a28d722b73aa2
m.magiera <m.magiera@avsystem.com>
2021-11-14 21:11:59 +0100

    spring init

---
+++ src/main/java/com/terator/terator/TeratorApplication.java
@@ -0,0 +1,13 @@
+package com.terator.terator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terator/terator/TeratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terator.terator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
